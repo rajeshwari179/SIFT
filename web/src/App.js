6a4bf5917c7a9a8882b93c3d1e4a316aa1204704
Kishore S. Shenoy <kichappa@gmail.com>
2024-04-21 23:42:25 -0400

    Resampling

--- web/src/App.js
+++ web/src/App.js
@@ -23,7 +23,8 @@ import {
 
 import epipolar from './images/dataset.jpeg';
 import structure from './images/gaussian.jpeg';
-import goldberg from './images/octave.jpeg';
+// import goldberg from './images/octave.jpeg';
+import DoG from './images/DoG.png';
 
 function useInterval(callback, delay) {
   const savedCallback = useRef();
@@ -147,7 +148,8 @@ function App() {
             </div>
           </div>
           <div className="bg_c_c">
-            0<div id="bg_Container_2" className="bg_Container">
+            0
+            <div id="bg_Container_2" className="bg_Container">
               <object
                 data={bgShapes2}
                 id="bgShapes_2"
@@ -184,8 +186,16 @@ function App() {
                   />
                 </div>
 
-                <div className="flex ltr full-width center">
-                  <h2 className="pfTitle">FAST SIFT</h2>
+                <div className="flex ttb full-width center">
+                  <h2 className="pfTitle half-bottom-margin">
+                    A GPU-Accelerated Fast SIFT
+                  </h2>
+
+                  <p className="pfContent width-control center-align no-top-margin">
+                    {/* Paragraph 1 */}A Scale-Invariant Feature Transform
+                    (SIFT) for CUDA enabled devices, with memory optimization
+                    and scope for large parallelization.
+                  </p>
                   {/* <div className="flex concept">
                     <p className="pfContent pad">Single video</p>
                   </div> */}
@@ -209,32 +219,57 @@ function App() {
                     <p className="pfContent pad">Stereo Pair</p>
                   </div> */}
                 </div>
+              </div>
+            </div>
+
+            <div id="pf_con_con">
+              <div id="pfs_container" className="medium">
                 <div id="introduction_section" className="medium">
                   <p className="pfTitle">Introduction</p>
                   <div className="row">
                     <div>
                       <p>
-                        <span className="head">
-
-                        </span>
+                        <span className="head"></span>
                       </p>
-                      <p>
-                        {/* Your introduction content goes here */}
-                      </p>
+                      <p>{/* Your introduction content goes here */}</p>
                     </div>
                   </div>
                 </div>
                 <p className="pfContent width-control">
                   {/* Paragraph 1 */}
-                  Our project focuses on accelerating the Scale-Invariant Feature Transform (SIFT) feature extraction process, a widely-used methodology for identifying interest points in images and videos. We aim to reduce the time required for this task by parallelizing various stages of the SIFT algorithm using CUDA-enabled GPUs. This approach substantially boosts the efficiency of video analysis, making it faster and more scalable compared to traditional methods.
-                  </p>
-                  <p className="pfContent width-control">
-                  The motivation for our project arises from the growing demand for efficient video analysis across industries such as surveillance, entertainment, and healthcare. Our GPU-accelerated solution addresses this need by delivering significant efficiency gains, leading to cost savings and improved productivity for organizations that rely on video analysis for decision-making, monitoring, or research purposes.
-                  </p>
-                  {/* Paragraph 3 */}
-                  <p className="pfContent width-control">For our system, the expected input is a video file, and the output consists of the coordinates or descriptors of the interest points detected in each frame. Specifically, our system will generate a list of interest points along with their corresponding features for each frame. This output facilitates further analysis, object recognition, or tracking tasks, enabling researchers, analysts, and professionals to extract valuable insights from videos more efficiently. By improving the speed and accuracy of video analysis, we aim to enhance the overall efficiency and productivity of video analysis workflows.
-                  </p>
-
+                  Our project focuses on accelerating the Scale-Invariant
+                  Feature Transform (SIFT) feature extraction process, a
+                  widely-used methodology for identifying interest points in
+                  images and videos. We aim to reduce the time required for this
+                  task by parallelizing various stages of the SIFT algorithm
+                  using CUDA-enabled GPUs. This approach substantially boosts
+                  the efficiency of video analysis, making it faster and more
+                  scalable compared to traditional methods.
+                </p>
+                <p className="pfContent width-control">
+                  The motivation for our project arises from the growing demand
+                  for efficient video analysis across industries such as
+                  surveillance, entertainment, and healthcare. Our
+                  GPU-accelerated solution addresses this need by delivering
+                  significant efficiency gains, leading to cost savings and
+                  improved productivity for organizations that rely on video
+                  analysis for decision-making, monitoring, or research
+                  purposes.
+                </p>
+                {/* Paragraph 3 */}
+                <p className="pfContent width-control">
+                  For our system, the expected input is a video file, and the
+                  output consists of the coordinates or descriptors of the
+                  interest points detected in each frame. Specifically, our
+                  system will generate a list of interest points along with
+                  their corresponding features for each frame. This output
+                  facilitates further analysis, object recognition, or tracking
+                  tasks, enabling researchers, analysts, and professionals to
+                  extract valuable insights from videos more efficiently. By
+                  improving the speed and accuracy of video analysis, we aim to
+                  enhance the overall efficiency and productivity of video
+                  analysis workflows.
+                </p>
               </div>
             </div>
             <div id="pf_con_con">
@@ -247,7 +282,8 @@ function App() {
                   <div>
                     <p>
                       <span className="head">
-                        <span className="lime">1 </span> Capturing Videos for Dataset Creation
+                        <span className="lime">1 </span> Capturing Videos for
+                        Dataset Creation
                       </span>
                     </p>
                     <p>
@@ -260,15 +296,20 @@ function App() {
                       determine corresponding points between frames, aiding in
                       the estimation of depth and facilitating the creation of a
                       stereoscopic effect. */}
-                      We curated our own dataset for the  project, focusing on the Coda building.
-                      To maintain clarity and sharpness across the frames of the video, we used a
-                      gimbal during recording. This ensured minimal blurring and stable footage throughout,
-                      enhancing the quality of the video dataset. For accurate camera calibration, we use
-                      various checkerboard patterns. These patterns serve as a calibration tool due to
-                      their well-defined geometry and known dimensions. The known properties of the checkerboard
-                      patterns provides a ground truth reference helping  in the precise estimation of camera parameters.
-                      To diversify our calibration dataset and avoid symmetrical patterns that could potentially skew the
-                      calibration results, we designed custom 3x3 grids.
+                      We curated our own dataset for the project, focusing on
+                      the Coda building. To maintain clarity and sharpness
+                      across the frames of the video, we used a gimbal during
+                      recording. This ensured minimal blurring and stable
+                      footage throughout, enhancing the quality of the video
+                      dataset. For accurate camera calibration, we use various
+                      checkerboard patterns. These patterns serve as a
+                      calibration tool due to their well-defined geometry and
+                      known dimensions. The known properties of the checkerboard
+                      patterns provides a ground truth reference helping in the
+                      precise estimation of camera parameters. To diversify our
+                      calibration dataset and avoid symmetrical patterns that
+                      could potentially skew the calibration results, we
+                      designed custom 3x3 grids.
                     </p>
                   </div>
                 </div>
@@ -276,21 +317,23 @@ function App() {
                   <div>
                     <p>
                       <span className="head">
-                        <span className="lime">2 </span> GAUSSIAN FILTERING
+                        <span className="lime">2 </span> Gaussian Filtering
                       </span>
                     </p>
                     <p>
-                      In our project, we utilized a Gaussian filter for image blurring
-                      due to its separable nature, which offers computational advantages.
-                      Traditional 2D convolution filters require nxm
-                      multiplications for each pixel, where n and m are
-                      the dimensions of the kernel. In contrast, separable filters can
-                      be split into two one-dimensional filters applied sequentially along
-                      rows and columns. By taking advantage of this property, we divided the
-                      blurring process into horizontal and vertical stages. This method reduces
-                      the computational load to just n + m  multiplications per pixel,
-                      making the Gaussian filter an efficient choice for image blurring while
-                      maintaining quality. <br />
+                      In our project, we utilized a Gaussian filter for image
+                      blurring due to its separable nature, which offers
+                      computational advantages. Traditional 2D convolution
+                      filters require nxm multiplications for each pixel, where
+                      n and m are the dimensions of the kernel. In contrast,
+                      separable filters can be split into two one-dimensional
+                      filters applied sequentially along rows and columns. By
+                      taking advantage of this property, we divided the blurring
+                      process into horizontal and vertical stages. This method
+                      reduces the computational load to just n + m
+                      multiplications per pixel, making the Gaussian filter an
+                      efficient choice for image blurring while maintaining
+                      quality. <br />
                       <br />
                       {/* Smith et al. utilize HDR cameras mounted on a mobile robot
                       for stereo vision-based 3D reconstruction{' '}
@@ -313,7 +356,7 @@ function App() {
                 </div>
                 <div className="row row-left">
                   <div className="img img-left">
-                    <img src={goldberg} alt="hello" />
+                    <img src={DoG} alt="hello" />
                   </div>
                   <div>
                     <p>
@@ -323,15 +366,77 @@ function App() {
                       </span>
                     </p>
                     <p>
-                      The Difference of Gaussian (DoG) method is employed to effectively identify stable keypoint locations within the scale space of an image. This is achieved by convolving the image with the difference-of-Gaussian function, D(x, y, σ) which is derived from the difference between two neighboring scales separated by a constant multiplicative factor k.One of the primary advantages of using this function is its computational efficiency. As the smoothed images
-                      L are already computed for scale space feature description, D can be easily obtained through straightforward image subtraction.
+                      The Difference of Gaussian (DoG) method is employed to
+                      effectively identify stable keypoint locations within the
+                      scale space of an image. This is achieved by convolving
+                      the image with the difference-of-Gaussian function, D(x,
+                      y, σ) which is derived from the difference between two
+                      neighboring scales separated by a constant multiplicative
+                      factor k.One of the primary advantages of using this
+                      function is its computational efficiency. As the smoothed
+                      images L are already computed for scale space feature
+                      description, D can be easily obtained through
+                      straightforward image subtraction.
                       <br />
                       <br />
-                      Moreover, the DoG function closely approximates the scale-normalized Laplacian of Gaussian. In practice, the initial image undergoes incremental convolution with Gaussians to generate images at different scales, spaced by a constant factor k
-                      n scale space. For efficient octave processing, each octave is divided into s intervals, setting k = 2^(1/s) To cover a complete octave during extrema detection, we produce s+3
-                      images in the blurred image stack for each octave. The DoG images, resulting from subtracting adjacent scales, are shown on the left. After processing a full octave, the Gaussian image with double the initial σ value is resampled by selecting every second pixel in each row and column. This resampling maintains accuracy relative to
-                      σ while significantly reducing computational overhead.
+                      Moreover, the DoG function closely approximates the
+                      scale-normalized Laplacian of Gaussian. In practice, the
+                      initial image undergoes incremental convolution with
+                      Gaussians to generate images at different scales, spaced
+                      by a constant factor k n scale space. For efficient octave
+                      processing, each octave is divided into s intervals,
+                      setting k = 2^(1/s) To cover a complete octave during
+                      extrema detection, we produce s+3 images in the blurred
+                      image stack for each octave. The DoG images, resulting
+                      from subtracting adjacent scales, are shown on the left.
+                      After processing a full octave, the Gaussian image with
+                      double the initial σ value is resampled by selecting every
+                      second pixel in each row and column. This resampling
+                      maintains accuracy relative to σ while significantly
+                      reducing computational overhead.
                     </p>
+                  </div>
+                </div>
+                
+                <div className="row row-right">
+                  <div>
+                    <p>
+                      <span className="head">
+                        <span className="lime">4 </span> Resampling
+                      </span>
+                    </p>
+                    <p>
+                      In our project, we utilized a Gaussian filter for image
+                      blurring due to its separable nature, which offers
+                      computational advantages. Traditional 2D convolution
+                      filters require nxm multiplications for each pixel, where
+                      n and m are the dimensions of the kernel. In contrast,
+                      separable filters can be split into two one-dimensional
+                      filters applied sequentially along rows and columns. By
+                      taking advantage of this property, we divided the blurring
+                      process into horizontal and vertical stages. This method
+                      reduces the computational load to just n + m
+                      multiplications per pixel, making the Gaussian filter an
+                      efficient choice for image blurring while maintaining
+                      quality. <br />
+                      <br />
+                      {/* Smith et al. utilize HDR cameras mounted on a mobile robot
+                      for stereo vision-based 3D reconstruction{' '}
+                      <span className="lime">[1]</span>. Their method captures
+                      textures and spatial features as 2D images and employs an
+                      algorithm for depth map visualization.
+                      <br />
+                      <br />
+                      OpenMVG is a library dedicated to Multiple-View Geometry
+                      and Structure-from-Motion tasks{' '}
+                      <span className="lime">[3]</span>. It facilitates
+                      identifying corresponding points between images and
+                      manipulating 3D geometry for various computer vision
+                      applications. */}
+                    </p>
+                  </div>
+                  <div className="img img-right">
+                    <img src={structure} alt="hello" />
                   </div>
                 </div>
               </div>
@@ -422,7 +527,17 @@ function App() {
                       </span>
                     </p>
                     <p>
-                      In our experiments, we generated a dataset comprising 1980 frames extracted from a 66-second video. To efficiently apply convolution with Gaussian blurring in both vertical and horizontal directions, we processed 128 images simultaneously, arranged in a row. This approach was designed to harness the high DRAM availability of modern GPUs. Specifically, we utilized an NVIDIA H100 80GB HBM3 GPU from the Georgia Tech's PACE cluster. With CUDA Version 12.2 and Julia 1.9.2, we leveraged the parallel processing capabilities of our GPU to achieve optimal performance.
+                      In our experiments, we generated a dataset comprising 1980
+                      frames extracted from a 66-second video. To efficiently
+                      apply convolution with Gaussian blurring in both vertical
+                      and horizontal directions, we processed 128 images
+                      simultaneously, arranged in a row. This approach was
+                      designed to harness the high DRAM availability of modern
+                      GPUs. Specifically, we utilized an NVIDIA H100 80GB HBM3
+                      GPU from the Georgia Tech's PACE cluster. With CUDA
+                      Version 12.2 and Julia 1.9.2, we leveraged the parallel
+                      processing capabilities of our GPU to achieve optimal
+                      performance.
                     </p>
                   </div>
                 </div>
